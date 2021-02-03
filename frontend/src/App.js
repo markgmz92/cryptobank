fbf455847eaca5bf3aea281a7b8a41b6a71d46a3
markgmz92 <markgmz92@gmail.com>
2021-02-03 20:42:28 +0800

    [CB - 003] Created header component and homescreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import HomeScreen from "./screens/HomeScreen/HomeScreen";
 
 function App() {
-  return <>Hello World</>;
+  return (
+    <Router>
+      <Header />
+      <Route path="/" component={HomeScreen} />
+    </Router>
+  );
 }
 
 export default App;
